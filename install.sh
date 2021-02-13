8dda6d81eae570fa3ee5ac50d41c445fb69281e5
mu <59917266+4eUeP@users.noreply.github.com>
2021-02-13 10:15:47 +0800

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+CABAL_BIN=${CABAL_BIN:-cabal}
+EXE_COMPONENT="$1"
+
+if [ -z "$EXE_COMPONENT" ]; then
+    echo "$0 <target>"
+else
+    $CABAL_BIN -- build --enable-executable-static --ghc-options=-split-sections
+    cp $(find dist-newstyle \( -name "$EXE_COMPONENT" -o -name "$EXE_COMPONENT" \) -type f) ~/.local/bin/
+fi
