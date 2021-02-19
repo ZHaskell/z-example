7bf6cfec7c762adc4ee1b184ddbb7d16bdfef277
mu <59917266+4eUeP@users.noreply.github.com>
2021-02-19 17:39:48 +0800

    Update SimpleFileSync

--- FileSystem/SimpleFileSync.hs
+++ FileSystem/SimpleFileSync.hs
@@ -10,35 +10,35 @@ import           Control.Concurrent      (forkIO)
 import           Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar,
                                           tryPutMVar)
 import           Control.Exception.Safe  (tryAny)
-import           Control.Monad           (join, void, when)
+import           Control.Monad           (void, when)
 import           GHC.Generics            (Generic)
 import qualified Z.Data.Builder          as Builder
 import           Z.Data.CBytes           (CBytes)
-import qualified Z.Data.CBytes           as ZC
-import qualified Z.Data.Vector           as ZV
-import qualified Z.Data.YAML             as YAML
-import           Z.IO.BIO                (runBIO, sinkToIO, (>|>))
-import qualified Z.IO.Environment        as Z
-import qualified Z.IO.FileSystem         as Z
+import qualified Z.Data.CBytes           as CBytes
+import           Z.Data.JSON             (JSON)
+import qualified Z.Data.Vector           as V
+import           Z.Data.YAML             (readYAMLFile)
+import           Z.IO.Environment        (getArgs)
+import           Z.IO.FileSystem         (normalize, watchDirs)
 import qualified Z.IO.Logger             as Log
-import qualified Z.IO.LowResTimer        as Z
-import qualified Z.IO.Process            as Z
+import           Z.IO.LowResTimer        (throttleTrailing_)
+import qualified Z.IO.Process            as Proc
 
 data Project = Project
   { src_path  :: CBytes
   , dest_path :: CBytes
   , ignores   :: [CBytes]
-  } deriving (Show, Generic, YAML.JSON)
+  } deriving (Show, Generic, JSON)
 
 main :: IO ()
 main = Log.withDefaultLogger $ do
-  argv <- Z.getArgs
+  argv <- getArgs
   if length argv /= 2
      then Log.fatal $ "No sush config file, run "
-                   <> (ZC.toBuilder $ argv !! 0) <> " <your-config-file-path>."
+                   <> (CBytes.toBuilder $ argv !! 0) <> " <your-config-file-path>."
      else do
        let configPath = argv !! 1
-       config <- YAML.readYAMLFile configPath
+       config <- readYAMLFile configPath
        projects <- mapM (\p -> (p, ) <$> newEmptyMVar) config
        if null projects
           then Log.fatal "Empty project!"
@@ -53,27 +53,28 @@ main = Log.withDefaultLogger $ do
 
 watchProject :: MVar () -> Project -> IO ()
 watchProject flagChange Project{..} = do
-  src <- join $ snd <$> Z.watchDirsRecursively [src_path]
   -- no matter how many FileChange events are popped, we will notify only once
   -- after (2/10)s.
-  throttledNotify <- Z.throttleTrailing_ 2 (void $ tryPutMVar flagChange ())
-  runBIO $ src >|> sinkToIO (const throttledNotify)
+  throttledNotify <- throttleTrailing_ 2 (void $ tryPutMVar flagChange ())
+  watchDirs [src_path] True $ const throttledNotify
 
 runRsync :: MVar () -> Project -> IO ()
 runRsync flagChange Project{..} = do
-  src_path' <- (<> "/") <$> Z.normalize src_path     -- add a trailing slash
-  dest_path' <- (<> "/") <$> Z.normalize dest_path   -- add a trailing slash
+  src_path' <- (<> "/") <$> normalize src_path     -- add a trailing slash
+  dest_path' <- (<> "/") <$> normalize dest_path   -- add a trailing slash
   let args = ["-azH", "--delete", "--partial"]
           ++ (concat $ map (\i -> ["--exclude", i]) ignores)
           ++ [src_path', dest_path']
 
   takeMVar flagChange
   Log.debug $ "Run: rsync "
-           <> foldr Builder.append "" (map ((<> " "). ZC.toBuilder) args)
-  (_out, err, _code) <- Z.readProcess
-    Z.defaultProcessOptions { Z.processFile = "rsync" , Z.processArgs = args }
+           <> foldr Builder.append "" (map ((<> " "). CBytes.toBuilder) args)
+  (_out, err, _code) <- Proc.readProcess
+    Proc.defaultProcessOptions { Proc.processFile = "rsync"
+                               , Proc.processArgs = args
+                               }
     ""
-  when (not $ ZV.null err) $ Log.fatal $ Builder.bytes err
+  when (not $ V.null err) $ Log.fatal $ Builder.bytes err
 
 foreverRun :: IO () -> IO ()
 foreverRun f = do
